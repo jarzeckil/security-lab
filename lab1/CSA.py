cdc960779615f6a701350a4c76045ef395440066
jarzeckil <jarzeckil@gmail.com>
2025-10-07 17:48:22 +0200

    update

--- lab1/CSA.py
+++
@@ -1,6 +0,0 @@
-def csa(text, key):
-    return 1
-
-a = 'a'
-
-print()
---
+++ lab1/encode.py
@@ -0,0 +1,23 @@
+def caesar(text, key):
+    result = ""
+    for i in range(len(text)):
+        if(text[i].islower()):
+            result += chr((ord(text[i]) - ord('a') + key)%26 + ord('a'))
+        elif(text[i].isupper()):
+            result += chr((ord(text[i]) - ord('A') + key)%26 + ord('A'))
+        else:
+            result += text[i]
+    return result
+
+
+with open("lab1/eng.txt", "r") as f:
+    text = f.read()
+
+text_encoded = caesar(text, 5)
+
+with open("lab1/eng_encoded.txt", "x") as f:
+    f.write(text_encoded)
+
+
+
+
